7e94fadb492afc21429c85e542b91272f1269c37
NurUddin111 <nuruddinmuhammad38@gmail.com>
2026-06-04 04:08:17 +0600

    feat: update proxy redirection logic for signup verification and password routes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -12,19 +12,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
-
-    {
-      url: `${APP_URL}/login`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-
-    {
-      url: `${APP_URL}/signup`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.9,
-    },
   ];
 }
